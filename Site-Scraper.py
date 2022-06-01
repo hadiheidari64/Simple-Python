637888485bb2f4c04c6cec108326ef50a789c72e
El3ct0r <91212532+hadiheidari64@users.noreply.github.com>
2022-06-01 16:28:54 -0700

    made it better and more professional :-)

--- Site-Scraper.py
+++ Site-Scraper.py
@@ -5,9 +5,16 @@
 # -lxml
 # -Selenium
 # lets use requests
-#put your website in the url
-
+# if you do not have the pyfiglet module installed remove it from the code
+# if you need pyfiglet installed copy this command in cmd line: #pip install pyfiglet
+import pyfiglet
 import requests
-url="https://yoursite.com/"
+# if you do not use pyfiglet module  so remove the next 2 lines, it is for showing the banner.
+ascii_banner = pyfiglet.figlet_format("WEBSCRAPER BY El3ct0r")
+print(ascii_banner)
+# input the website
+# make sure that you input in this format https://yoursite.com/
+url= input("input the website: ")
+
 responses = requests.get(url)
 print(responses.text)
